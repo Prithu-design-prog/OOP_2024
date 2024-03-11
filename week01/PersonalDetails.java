67d089b78e851a64ee1848a477b29a9c2d3f84b3
Prithu-design-prog <prithuadhikari34@gmail.com>
2024-03-11 12:31:11 +0545

    task 2

---
+++ week01/PersonalDetails.java
@@ -0,0 +1,15 @@
+// 3. Create a second project called “PersonalDetails” and write a program that outputs some personal details to the screen, e.g. Name, Address, Age, Phone number etc.
+
+public class PersonalDetails {
+    public static void main(String args[]) {
+        String name = "Prithu";
+        int age = 19;
+        long phone = 9875678901L; // Use long for large numbers and suffix with 'L'
+        String address = "bharatpur";
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(phone);
+        System.out.println(address);
+    }
+}
